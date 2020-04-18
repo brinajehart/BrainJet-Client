8337766963a657720db134a8dcd804dc2a48abe6
JanezSedeljsak <janez.sedeljsak@gmail.com>
2020-04-18 20:41:33 +0200

    edit updates

--- src/components/TaskEdit.vue
+++ src/components/TaskEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card style="width: 100%">
-    <v-form>
+    <v-form v-model="valid">
       <v-container>
         <v-row>
           <v-col cols="12" md="4" style="margin-top:15px">
@@ -33,6 +33,78 @@
                     bullist numlist outdent indent | removeformat | help'
             }"
             />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-row>
+              <h2 style="margin: 0 10px 0 15px;" class="display-1 font-weight-light">Subtasks</h2>
+              <v-btn v-tooltip="'Add Subtask'" color="primary" @click="addSubtask">Add</v-btn>
+            </v-row>
+            <v-simple-table dense>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Title</th>
+                    <th class="text-left">Status</th>
+                    <th class="text-left">Done By</th>
+                    <th class="text-left">Date Completed</th>
+                    <th class="text-right">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(item,key) in form.subtasks" :key="key">
+                    <td>
+                      <v-text-field style="margin-top:15px" v-model="form.title" label="Title" required></v-text-field>
+                    </td>
+                    <td>
+                      <v-text-field style="margin-top:15px" v-model="form.title" label="Title" required></v-text-field>
+                    </td>
+                    <td>
+                      <v-text-field style="margin-top:15px" v-model="form.title" label="Title" required></v-text-field>
+                    </td>
+                    <td>
+                      <datepicker v-model="form.due_date" name="due_date" style="width: 100%"></datepicker>
+                    </td>
+                    <td class="text-right">
+                      <v-btn small color="error" @click="$router.go(-1)">Delete</v-btn>
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-row>
+              <h2 style="margin: 0 10px 0 15px;" class="display-1 font-weight-light">Collaborators</h2>
+              <v-btn v-tooltip="'Add Collaborator'" color="primary">Add</v-btn>
+            </v-row>
+            <v-simple-table dense>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Name</th>
+                    <th class="text-left">Permissions</th>
+                    <th class="text-right">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(item,key) in form.collaborators" :key="key" style="height: 50px">
+                    <td>
+                      <v-text-field v-model="form.title" label="Title" required></v-text-field>
+                    </td>
+                    <td>
+                      <v-text-field v-model="form.title" label="Title" required></v-text-field>
+                    </td>
+                    <td class="text-right">
+                      <v-btn small color="error" @click="$router.go(-1)">Delete</v-btn>
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
           </v-col>
         </v-row>
         <v-row>
